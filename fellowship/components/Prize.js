b7b4f66a698f0b712dc0d251e4475d3a964eebf5
drumfeet <drumfeet@gmail.com>
2023-12-03 16:46:21 +0800

    Add Prize component

---
+++ fellowship/components/Prize.js
@@ -0,0 +1,48 @@
+import { Flex, Heading, Text, Box } from "@chakra-ui/react"
+import { PrizePoolIcon, PrizePoolBackground } from "./images/icons/prize"
+
+export default function Prize() {
+  return (
+    <>
+      <Flex justifyContent="center" position="relative">
+        <Flex
+          position="absolute"
+          left="5"
+          bottom="10"
+          display={{ base: "none", md: "flex" }}
+        >
+          <PrizePoolIcon />
+        </Flex>
+
+        <Flex
+          flexDirection="column"
+          gap="32px"
+          alignItems="center"
+          paddingY="79px"
+        >
+          <Heading
+            fontStyle="italic"
+            fontWeight="600"
+            style={{ fontSize: "clamp(3rem, 5vw, 6rem);" }}
+          >
+            $ 22,000
+          </Heading>
+          <Text
+            fontWeight="500"
+            style={{ fontSize: "clamp(1rem, 5vw, 2rem);" }}
+          >
+            Prize pool
+          </Text>
+        </Flex>
+
+        <Flex
+          display={{ base: "none", md: "flex" }}
+          position="absolute"
+          right="0"
+        >
+          <PrizePoolBackground />
+        </Flex>
+      </Flex>
+    </>
+  )
+}
